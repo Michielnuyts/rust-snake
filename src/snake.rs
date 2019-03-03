98d9dcf1b1a638834bac0a371b909d3fda38643c
Michiel Nuyts <michiel.nuyts@neanex.com>
2019-03-03 12:51:04 +0100

    first commit

---
+++ src/snake.rs
@@ -0,0 +1,7 @@
+use std::collections::LinkedList;
+use piston_window::{Coontext, G2d};
+use piston_window::types::Color;
+
+use draw::draw_block;
+
+const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
